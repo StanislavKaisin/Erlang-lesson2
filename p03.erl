afceeb23b5aa62d0c5306951e02420149be1ecc1
StanislavKaisin <8050361@gmail.com>
2020-10-29 23:17:03 +0200

    fixed p03

--- p03.erl
+++ p03.erl
@@ -3,5 +3,7 @@
 
 element_at([H|_], 1)->
     H;
-  element_at([_|T], Index)->
-    element_at(T, Index-1). 
+element_at([_|T], Index)->
+    element_at(T, Index-1);
+element_at([], _)->
+  undefined.
